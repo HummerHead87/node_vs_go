cea577e561fd54e2ff1e894d7cd552d5cedcebf4
Valeyev Rustam <snooks87@gmail.com>
2019-08-10 20:26:47 +0300

    benchmarking go formatInput func

--- main.go
+++ main.go
@@ -28,14 +28,83 @@ type BinanceSymbol struct {
 	price float64
 }
 
+type BencmarkResult struct {
+	min time.Duration
+	max time.Duration
+	avg time.Duration
+}
+
+func (b BencmarkResult) String() string {
+	return fmt.Sprintf("min: %s, max: %s, avg: %s", b.min, b.max, b.avg)
+}
+
 func main() {
 	prices, err := getBinanceExchanges()
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	start := time.Now()
+	// start := time.Now()
 
+	// result := formatInput(prices)
+	// fmt.Println(time.Since(start))
+	// fmt.Println(result)
+	fmt.Println(benchMark(1000, formatInput, prices))
+}
+
+func benchMark(iterations int, f func([]*binance.SymbolPrice) []BinanceSymbol, args []*binance.SymbolPrice) BencmarkResult {
+	times := make([]time.Duration, iterations)
+
+	for i := 0; i < iterations; i++ {
+		start := time.Now()
+		result := f(args)
+		if result != nil {
+			times[i] = time.Since(start)
+		}
+	}
+
+	return BencmarkResult{
+		min: minDuration(times),
+		max: maxDuration(times),
+		avg: avgDuration(times),
+	}
+}
+
+func minDuration(values []time.Duration) time.Duration {
+	min := values[0]
+	for _, v := range values {
+		if v < min {
+			min = v
+		}
+	}
+
+	return min
+}
+
+func maxDuration(values []time.Duration) time.Duration {
+	max := values[0]
+	for _, v := range values {
+		if v > max {
+			max = v
+		}
+	}
+
+	return max
+}
+
+func avgDuration(values []time.Duration) time.Duration {
+	var summ time.Duration
+
+	for _, v := range values {
+		summ += v
+	}
+
+	avg := int(summ/time.Microsecond) / len(values)
+	// return summ / (len(values) * time.Microsecond)
+	return time.Duration(avg) * time.Microsecond
+}
+
+func formatInput(prices []*binance.SymbolPrice) []BinanceSymbol {
 	result := make([]BinanceSymbol, len(prices))
 
 	for i, p := range prices {
@@ -48,8 +117,8 @@ func main() {
 	}
 
 	sort.Slice(result, func(i, j int) bool { return result[i].pair < result[j].pair })
-	fmt.Println(time.Since(start))
-	// fmt.Println(result)
+
+	return result
 }
 
 func _splitSymbolStr(symbol string) string {
@@ -74,7 +143,9 @@ func _splitSymbolStr2(symbol string) string {
 		if strings.HasSuffix(symbol, coin) {
 			i := utf8.RuneCountInString(symbol) - utf8.RuneCountInString(coin)
 			symbolSlice := []rune(symbol)
-			return string(append(symbolSlice[:i], append([]rune{'_'}, symbolSlice[i:]...)...))
+			result = string(append(symbolSlice[:i], append([]rune{'_'}, symbolSlice[i:]...)...))
+			// result = fmt.Sprintf("%s_%s", string(symbolSlice[:i]), string(symbolSlice[i:]))
+			break
 		}
 	}
 
